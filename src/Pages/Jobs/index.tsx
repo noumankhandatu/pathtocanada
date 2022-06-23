a928eedac3c45ebf69cc765b605eb5826fac470b
nouman khan <noumankhandatu>
2022-06-23 10:13:39 +0500

    stepper added

--- src/Pages/Jobs/index.tsx
+++ src/Pages/Jobs/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import HomePageFirstSection from "../../components/Organism/homePageFirstSection/index";
 import BlackBannerLeafSection from "../../components/Organism/blackBannerLeafSection/index";
 import Wrapper from "../../components/Organism/Wrapper/index";
+import Stepper from "../../components/Molecules/stepper";
+import StepperArray from "../../components/array/stepperArray";
 const Jobs = () => {
   return (
     <div className="pt-40">
@@ -11,8 +13,22 @@ const Jobs = () => {
         bgImage="https://path2canada.ca/wp-content/uploads/2022/05/jobs_hero.svg"
         showFirstBtn={true}
       />
+      <Wrapper className="bg-aqua-color pt-20 pb-20 text-white text-center">
+        <p className="text-5xl font-bold">How we help you get a Canadian job</p>
+        <div className="pt-20 flex">
+          {StepperArray.map((items: object) => {
+            return (
+              <Stepper
+                count={1}
+                heading="Submit your profile to see if you qualify"
+                paragraph="Fill out the “Candidate Assessment” form to see if you could qualify for a Canadian work visa (via Global Talent Stream) and so we can learn about your tech education and work experience."
+              />
+            );
+          })}
+          
+        </div>
+      </Wrapper>
       <BlackBannerLeafSection />
-      <Wrapper className="bg-aqua-color">adad</Wrapper>
     </div>
   );
 };
---
+++ src/components/array/stepperArray.ts
@@ -0,0 +1,24 @@
+const StepperArray = [
+  {
+    count: 1,
+    heading: "hello one",
+    paragraph: "hello paragraph",
+  },
+  {
+    count: 1,
+    heading: "hello one",
+    paragraph: "hello paragraph",
+  },
+  {
+    count: 1,
+    heading: "hello one",
+    paragraph: "hello paragraph",
+  },
+  {
+    count: 1,
+    heading: "hello one",
+    paragraph: "hello paragraph",
+  },
+];
+
+export default StepperArray;
